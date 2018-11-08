53698f201895bbf5dd4f61dc8eb9f4091c2c3b56
Tim Brattberg <timbrattberg@gmail.com>
2018-11-08 07:50:24 -0800

    Champ Select Starting to Work

--- etest/bug.js
+++ etest/bug.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+
 let browserWindow = electron.remote.getCurrentWindow()
 
 var fs = require('fs');
@@ -24,19 +25,37 @@ function processFile() {
     var res = lockfile.split(":");
     port = res[2];
     password = res[3];
+/*
+    (function() {
+     	var x = new XMLHttpRequest();
+   //  	var recent = document.getElementById('recent');
+     	x.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200)
+   //       recent.innerHTML = x.response;
+           console.log('request');
+          console.log(x.response);
+        }
+     	x.open("GET", "https://127.0.0.1:"+port+"/lol-champ-select/v1/session");
+     	x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+       x.setRequestHeader("Authorization", "Basic " + btoa("riot:"+password));
+       x.setRequestHeader("Accept", "application/json");
+       console.log('requeststart');
+ //      console.log(x);
+     	x.send();
+     })();
+*/
 }
 
 var static;
 
 (function() {
-var x = new XMLHttpRequest();
-x.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) {
-    static = JSON.parse(x.response);
+  var x = new XMLHttpRequest();
+  x.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) {
+      static = JSON.parse(x.response);
+    }
   }
-}
-x.open("POST", "json/full.json");
-x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-x.send();
+  x.open("POST", "json/full.json");
+  x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  x.send();
 })();
 
 
@@ -44,26 +63,39 @@ x.send();
 var picks;
 var chosen = new Object();
 function readPicks() {
-var x = new XMLHttpRequest();
-x.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-  picks = JSON.parse(x.response);
-  Object.keys(picks.actions[0]).forEach(function(key) {
-    if (picks.actions[0][key].type == 'pick') chosen[picks.actions[0][key].actorCellId] = picks.actions[0][key].championId;
-  });
-  Object.keys(chosen).forEach(function(key) {
-    var player = document.getElementById('player'+i);
-    if (typeof chosen[i] != 'undefined') {
-      var id = chosen[i];
-      player.innerHTML = '<img src="http://ddragon.leagueoflegends.com/cdn/8.22.1/img/champion/'+static.champion.id[id].key+'.png">';
+  var x = new XMLHttpRequest();
+  x.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+//      console.log(x.response);
+    picks = JSON.parse(x.response);
+    Object.keys(picks.actions[0]).forEach(function(key) {
+      if (picks.actions[0][key].type == 'pick') chosen[picks.actions[0][key].actorCellId] = picks.actions[0][key].championId;
+    });
+//    Object.keys(chosen).forEach(function(key) {
+    for (var i=0; i<10; i++) {
+      var player = document.getElementById('player'+i);
+      if (typeof chosen[i] != 'undefined') {
+        var id = chosen[i];
+        console.log(i+' '+id);
+        if (id) player.innerHTML = '<img src="http://ddragon.leagueoflegends.com/cdn/8.22.1/img/champion/'+static.champion.id[id].key+'.png">';
+      } else {
+        player.innerHTML = 'P'+i+' 404';
+      }
     }
-  });
-  console.log(chosen);
+    console.log(chosen);
+    }
+    if (this.readyState == 4 && this.status === 404) {
+          console.log('Not in Champion Select');
+    }
   }
-}
-x.open("POST", "http://app/pick.php?"+Math.random());
-x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-x.send();
+  x.open("GET", "https://127.0.0.1:"+port+"/lol-champ-select/v1/session");
+  x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   x.setRequestHeader("Authorization", "Basic " + btoa("riot:"+password));
+   x.setRequestHeader("Accept", "application/json");
+  /*
+  x.open("POST", "http://app/pick.php?"+Math.random());
+  x.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); */
+  x.send();
 };
 
 setInterval(function(){ readPicks(); }, 1000);
--- etest/index.js
+++ etest/index.js
@@ -1,14 +1,24 @@
 const {app, BrowserWindow} = require('electron')
 
+app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+      // On certificate error we disable default behaviour (stop loading the page)
+      // and we then say "it is all fine - true" to the callback
+      event.preventDefault();
+      callback(true);
+  });
+
 app.on('ready', ()=>{
   let browser = new BrowserWindow({
     width: 500,
     height: 500,
     transparent: true,
     frame: false,
-    alwaysOnTop: true
+    alwaysOnTop: true,
   })
-  
+
+  app.commandLine.appendSwitch('ignore-certificate-errors', 'true');
+
+
   browser.loadFile(`${__dirname}/bug.htm`)
 
   browser.webContents.openDevTools({mode: 'detach'})
